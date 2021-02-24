36135a13d3ad10eabb5184a80c8deb12a5409c14
SGarcia710 <sgarcia710@outlook.com>
2021-02-24 00:49:15 -0500

    Clean up and indexing folders

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import {HomeScreen} from './src/screens';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
+        <HomeScreen />
         <AntDesign name="star" size={16} color="black" />
       </SafeAreaView>
     </>
